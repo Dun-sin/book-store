fa3f6cee00cdc6f3668a32d52b1fe2471e3b9cb4
Favour Faiyetole <jesudunsinfaiye@gmail.com>
2021-12-30 13:53:38 +0100

    Book routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -23,13 +24,14 @@ const db = mongoose.connection;
 
 db.on('error', error => {
   console.log(error)
-})
+});
 
 db.once('open', () => {
   console.log('Connected to Mongoose')
-})
+});
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000); 
